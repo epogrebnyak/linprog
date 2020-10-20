7017c74b9136205fe54e6bc0bf79d2dc291b3aa9
Evgeniy <e.pogrebnyak@gmail.com>
2020-10-20 23:51:03 +0300

    Update task3.py

--- task3.py
+++ task3.py
@@ -1,22 +1,3 @@
-"""Task description:
-
-- 1 product
-- planning for 7 days of production
-- production quantaties x[t], t = 0...6
-- max production capacity C, 0 <= x[t] <= C, let C = 5
-- product can be stored for s days, let s = 3
-- we are given a purchases schedule purchases[t], let purchases = [0, 0, 2, 8, 1, 0, 1]
-- cumulative sum of purchases is required stock of product for day t
-- find x[t] (can be non-unique)
-
-Demonstrate solver behavior under following parameters and expected results:
-
-- all orders are under max capacity per day, no need to make stocks ps = [1, 2, 5, 2, 1, 5, 3]
-- production schedule is unique, eg when ps = [0, 0, 15, 0, 0, 0, 0] and xs = [5,5,5,0, 0, 0, 0]
-- production schedule not unique (as above)
-- production schedule not feasisble eg ps = [0, 0, 0, 20, 0, 0, 0]
-"""
-
 import time
 import warnings
 from contextlib import contextmanager
@@ -28,20 +9,24 @@ from numpy import cumsum, array
 
 warnings.simplefilter("ignore")  # shut <Spaces are not permitted in the name.>
 
+
 @dataclass
 class Order:
-    day: int # zero-based
-    quantity: float    
+    day: int  # zero-based
+    quantity: float
+
 
 # Given:
 
 max_output = 5
-orders = [Order(0, 1), 
-          Order(1, 3), 
-          Order(1, 2), 
-          Order(1, 0.5), 
-          Order(2, 0), 
-          Order(3, 5)]
+orders = [
+    Order(0, 1),
+    Order(1, 3),
+    Order(1, 2),
+    Order(1, 0.5),
+    Order(2, 0),
+    Order(3, 5),
+]
 
 total_days = 1 + max([order.day for order in orders])
 days = [i for i in range(total_days)]
@@ -62,26 +47,29 @@ for d in days:
 def accumulate(var, i):
     return pulp.lpSum([var[k] for k in range(i + 1)])
 
+
 for d in days:
     inventory[d] = accumulate(x, d) - accumulate(purchases, d)
-    model += accumulate(x, d) - accumulate(purchases, d) >=0
+    model += accumulate(x, d) - accumulate(purchases, d) >= 0
 
 # Closed sum
 model += pulp.lpSum(x) == pulp.lpSum(purchases)
 
 # Target
-model += pulp.lpSum([purchases[d] for d in days]) - \
-         0.5 * pulp.lpSum([inventory[d] for d in days])
-    
+model += pulp.lpSum([purchases[d] for d in days]) - 0.5 * pulp.lpSum(
+    [inventory[d] for d in days]
+)
 
 
 feasibility = model.solve()
 print("Status:", pulp.LpStatus[feasibility])
+
 
 def peek(x):
     """
     Lookup into dict of pulp.LpVariable.
     """
     return [v.value() for v in x.values()]
+
 
 print(peek(x))
